099226b95188ffedc0f0ae9aac3828763688cae2
Zakaria Airacaz <zakaria.oussalem.pro@gmail.com>
2023-01-18 15:40:18 +0100

    solved adding two linked lists

---
+++ src/LeetCode/LeetCode_medium.py
@@ -0,0 +1,80 @@
+# Definition for singly-linked list.
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+    def insert_end(self, val):
+        if self.val is None:
+            self.val = val
+            return
+
+        iterator = self
+
+        while iterator.next:
+            iterator = iterator.next
+
+        iterator.next = Node(val)
+
+    def insert_values(self, values):
+        if values:
+            for e in values:
+                self.insert_end(e)
+
+    def __eq__(self, second: object) -> bool:
+
+        it1 = self
+        it2 = second
+        while it1 or it2:
+            if not it1:
+                return False
+            if not it2:
+                return False
+            if it1.val == it2.val:
+                it1 = it1.next
+                it2 = it2.next
+            else:
+                return False
+        return True
+
+    def print(self):
+        """String representation of the linked lists"""
+
+        toPrint = ""
+        iterator = self
+
+        while iterator.next is not None:
+            toPrint += str(iterator.val) + " -- "
+            iterator = iterator.next
+        toPrint += str(iterator.val) + " -- "
+        return toPrint.strip()
+
+
+# addTwoNumbers
+def addTwoNumbers(l1: Node, l2: Node) -> Node:
+
+    if l1.val is None:
+        return l2
+    if l2.val is None:
+        return l1
+
+    carry = l1.val + l2.val
+    result = Node(val=carry % 10)
+    iterator = result
+    carry = carry // 10
+    l1 = l1.next
+    l2 = l2.next
+    while l1 or l2 or carry:
+        iterator.next = Node()
+        iterator = iterator.next
+        if l1:
+            carry = l1.val + carry
+            l1 = l1.next
+        if l2:
+            carry = l2.val + carry
+            l2 = l2.next
+
+        iterator.val = carry % 10
+        carry = carry // 10
+
+    return result
---
+++ tests/test_LeetCode/test_medium.py
@@ -0,0 +1,25 @@
+from LeetCode.LeetCode_medium import addTwoNumbers, Node
+import pytest
+
+
+def test_Node_equal():
+    l1 = Node()
+    l1.insert_values([1, 2, 3])
+    assert l1 == l1
+
+
+@pytest.mark.parametrize(
+    "input_1, input_2, expected",
+    [([1, 0, 2], [2, 4, 6], [3, 4, 8])],
+)
+def test_add_binary(input_1, input_2, expected):
+    l1 = Node()
+    l2 = Node()
+    l1.insert_values(input_1)
+    l2.insert_values(input_2)
+    exp = Node()
+    exp.insert_values(expected)
+    print(l1.print())
+    print("")
+    print(l2.print())
+    assert addTwoNumbers(l1, l2) == exp
