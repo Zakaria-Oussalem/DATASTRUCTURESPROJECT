867ac178985ef37b9de55aaec0e0be532f9de614
Zakaria Airacaz <zakaria.oussalem.pro@gmail.com>
2023-02-12 18:18:48 +0100

    Final touches on basic graph functionnality

---
+++ src/Graphs/graphs.py
@@ -0,0 +1,61 @@
+# Implementing the graph class
+import numpy as  np
+
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def add_neighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return f"{self.id} is connected to: {str([x.id for x in self.connected])}"
+
+    def get_nodes(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self, nbr):
+        return self.connectedTo.get(nbr)
+
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def add_vertex(self, key) -> None:
+        newVertex = Vertex(key)
+        self.vertList[newVertex.id] = newVertex
+        self.numVertices += 1
+
+    def getVertex(self, key):
+        return self.vertList.get(key)
+
+    def __contains__(self, key):
+        return key in self.vertList
+
+    def addEdge(self, f, t, weight=0):
+
+        if f not in self:
+            self.add_vertex(f)
+        if t not in self:
+            self.add_vertex(t)
+        self.vertList[f].add_neighbor(t, weight)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def getCount(self):
+        return self.numVertices
+
+    def getAdjencencyMatrix(self):
+        adj_matrix = np.zeros(self.getCount(),self.getCount())
+        for v1 in range(self.getCount()):
+            for v2 in range(self.getCount()):
+                if self.getVertex(v1).getWeight(v2):
+                    adj_matrix[v1, v2] = self.getVertex(v1).getWeight(v2)
+        return adj_matrix
